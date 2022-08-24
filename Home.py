18f9605210a6b32e1cd4ba8a738fee02ce3521f1
pykto <alanr15.lll@gmail.com>
2022-08-23 18:22:36 -0600

    title fixed

--- Home.py
+++ Home.py
@@ -6,8 +6,8 @@ MEDIA_DIR = os.path.join(os.getcwd(), 'media', 'Home')
 
 ### Page content
 
-col1, mid, col2 = st.beta_columns([1,1,20])
+col1, mid, col2 = st.columns([1,1,20])
 with col1:
-    st.image(os.path.join(MEDIA_DIR, 'img', 'CUU_sticker_blanco1.png'), width=200)
+    st.image(os.path.join(MEDIA_DIR, 'img', 'CUU_sticker_blanco1.png'), width=100)
 with col2:
     st.title('NASA Space Apps Chihuahua')
