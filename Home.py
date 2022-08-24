1a0aac7d625953e4427c432b567571801df30fcd
pykto <alanr15.lll@gmail.com>
2022-08-23 18:08:13 -0600

    img fixed

--- Home.py
+++ Home.py
@@ -6,5 +6,5 @@ MEDIA_DIR = os.path.join(os.getcwd(), 'media', 'Home')
 
 ### Page content
 
-st.image(os.path.join(MEDIA_DIR, 'img', 'CUU_negro_rocketred.png'))
+st.image(os.path.join(MEDIA_DIR, 'img', 'CUU_sticker_blanco1.png'), width=200)
 st.title('NASA Space Apps Chihuahua')
