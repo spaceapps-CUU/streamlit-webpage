e06110da04a9c94f2513d29b18bcaf347f285f42
pykto <alanr15.lll@gmail.com>
2022-08-23 18:04:56 -0600

    logo

--- Home.py
+++ Home.py
@@ -1,3 +1,10 @@
 import streamlit as st
+import os
+## Page variables
 
+MEDIA_DIR = os.path.join(os.getcwd(), 'media', 'Home') 
+
+### Page content
+
+st.image(os.join(MEDIA_DIR, 'img', 'CUU_negro_rocketred.png'))
 st.title('NASA Space Apps Chihuahua')
