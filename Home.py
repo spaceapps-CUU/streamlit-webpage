d4bd5f15ac18b1d72fee87cf75c0d3be6b766a9d
pykto <alanr15.lll@gmail.com>
2022-08-23 18:31:39 -0600

    fixed title

--- Home.py
+++ Home.py
@@ -6,7 +6,7 @@ MEDIA_DIR = os.path.join(os.getcwd(), 'media', 'Home')
 
 ### Page content
 
-col1, mid, col2 = st.columns([1,1,30])
+col1, mid, col2 = st.columns([1,1,10])
 with col1:
     st.image(os.path.join(MEDIA_DIR, 'img', 'CUU_sticker_blanco1.png'), width=150)
 with col2:
